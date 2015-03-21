5f90fa93168df6b9104963abda025f53d3b00eb1
Kushal Jayswal <ku@kutec.co.in>
2015-03-21 22:13:53 +0530

    Added dynamic mechanism on click

--- js/main.js
+++ js/main.js
@@ -4,34 +4,42 @@
 		// Configuration
 		var settings = $.extend({
 			// These are the defaults.
-			message		: "This button fires only in devices.",
-			bgColor		: "#090",
-			container	: ".post",
-			target		: "h2"
+			message			: "This button fires only in devices.",
+			bgColor			: "#090",
+			container		: ".post",
+			target			: "h2"
 		}, options );
- 
-		// Variables
-		var el 			= $(this);
-		var boxes		= el.find(settings.container);
-		var targetTo	= boxes.find(settings.target);
-		var aTag		= targetTo.find('a');
-		var devices		= /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-		
-		if(aTag && devices){
+ 		
+		this.each(function(){
+			// Variables
+			var el 			= $(this);
+			var box			= el.find(settings.container);
+			var targetTo	= box.find(settings.target);
+			var aTag		= targetTo.find('a');
+			var devices		= /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 			
-			// get data
-			var url 	= aTag.attr('href');
-			var txt 	= targetTo.text();
-			
-			// appending WhatsApp Sharing button to container
-			boxes.append("<a href='whatsapp://send' class='wa_btn wa_btn_s' data-text='"+txt+"' data-href='"+url+"'><i class='fa fa-whatsapp'></i> Share</a>");
-		}
-		else{
-			boxes.on("click",".wa_btn", function() {
-				console.log(settings.message);	
+			if(aTag){
+				// appending WhatsApp Sharing button to container
+				box.append("<a href='#' class='wa_btn wa_btn_s' data-text='' data-href=''><i class='fa fa-whatsapp'></i> Share</a>");
+			}
+			/*else{
+				boxes.on("click",".wa_btn", function() {
+					alert(settings.message);	
+				});
+			}*/
+			box.on('click','.wa_btn',function(){
+				var url 	= $(this).parent(box).find(aTag).attr('href');
+				var txt		= $(this).parent(box).find(targetTo).text();
+				var wmsg	= encodeURIComponent(txt)+" - "+encodeURIComponent(url);
+				var wurl	= 'whatsapp://send?text='+wmsg;
+				
+				$(this).attr('href',wurl);
+				$(this).attr('data-href',url);
+				$(this).attr('data-text',txt);
+				
 			});
-		}
- 
+			
+		});
     };
  
 }( jQuery ));
